6b3d483af8c0cfebe81aeb8f8d60f19f9fd9a505
Ryan Fielding <rafieldi@sfu.ca>
2020-07-21 14:36:19 -0700

    updates to self.agent feed,step,train placement

--- src/bots/rlplayer_nfsp_train.py
+++ src/bots/rlplayer_nfsp_train.py
@@ -24,6 +24,9 @@ class RLPlayer(BasePokerPlayer):
         super().__init__()
         self.wins = 0
         self.losses = 0
+        '''
+        Instantiate agent.
+        '''
         # Setup RL NFSP agent
         # Set the iterations numbers and how frequently we evaluate/save plot
         evaluate_every = 10000
@@ -38,7 +41,7 @@ class RLPlayer(BasePokerPlayer):
         # Set a global seed
         set_global_seed(0)
         # Set agent - TODO - determine PPE parameters
-        agent = NFSPAgent(scope='nfsp',
+        self.agent = NFSPAgent(scope='nfsp',
                             action_num=3,
                             state_shape=54,
                             hidden_layers_sizes=[512,512],
@@ -49,13 +52,11 @@ class RLPlayer(BasePokerPlayer):
                             q_mlp_layers=[512,512],
                             device=torch.device('cpu'))
         # Init a Logger to plot the learning curve
-        logger = Logger(log_dir)
-
+        self.logger = Logger(log_dir)
+        
     # example from MC player - TODO
     def declare_action(self, valid_actions, hole_card, round_state):
-        # Estimate the win rate
-        win_rate = estimate_win_rate(self.n_simulations, self.n_players, hole_card, round_state['community_card'])
-
+        
         # Check whether it is possible to call
         can_call = len([item for item in valid_actions if item['action'] == 'call']) > 0
         if can_call:
@@ -65,48 +66,87 @@ class RLPlayer(BasePokerPlayer):
             call_amount = 0
 
         amount = None
-
-        # If the win rate is large enough, then raise
-        if win_rate > 0.5:
-            raise_amount_options = [item for item in valid_actions if item['action'] == 'raise'][0]['amount']
-            if win_rate > 0.85:
-                # If it is extremely likely to win, then raise as much as possible
-                action = 'raise'
-                amount = raise_amount_options['max']
-            elif win_rate > 0.75:
-                # If it is likely to win, then raise by the minimum amount possible
-                action = 'raise'
-                amount = raise_amount_options['min']
-            else:
-                # If there is a chance to win, then call
-                action = 'call'
-        else:
-            action = 'call' if can_call and call_amount == 0 else 'fold'
-
         # Set the amount
         if amount is None:
             items = [item for item in valid_actions if item['action'] == action]
             amount = items[0]['amount']
-
+        
+        # feed forward pass through agent NN
+        action = self.agent.step()
         return action, amount
 
-    # Encode state data here upon receiving game/round updates
+        # From MonteCarlo for ref
+        # # Estimate the win rate
+        # win_rate = estimate_win_rate(self.n_simulations, self.n_players, hole_card, round_state['community_card'])
+
+        # # Check whether it is possible to call
+        # can_call = len([item for item in valid_actions if item['action'] == 'call']) > 0
+        # if can_call:
+        #     # If so, compute the amount that needs to be called
+        #     call_amount = [item for item in valid_actions if item['action'] == 'call'][0]['amount']
+        # else:
+        #     call_amount = 0
+
+        # amount = None
+
+        # # If the win rate is large enough, then raise
+        # if win_rate > 0.5:
+        #     raise_amount_options = [item for item in valid_actions if item['action'] == 'raise'][0]['amount']
+        #     if win_rate > 0.85:
+        #         # If it is extremely likely to win, then raise as much as possible
+        #         action = 'raise'
+        #         amount = raise_amount_options['max']
+        #     elif win_rate > 0.75:
+        #         # If it is likely to win, then raise by the minimum amount possible
+        #         action = 'raise'
+        #         amount = raise_amount_options['min']
+        #     else:
+        #         # If there is a chance to win, then call
+        #         action = 'call'
+        # else:
+        #     action = 'call' if can_call and call_amount == 0 else 'fold'
+
+        # # Set the amount
+        # if amount is None:
+        #     items = [item for item in valid_actions if item['action'] == action]
+        #     amount = items[0]['amount']
+
+        # return action, amount
+        
+    # Encode state data here upon receiving game/round updates and put in trajectory
+    # Need to set agent.feed(ts) somewhere
     def receive_game_start_message(self, game_info):
         self.n_players = game_info['player_num']
+        self.trajectory = 0 # placeholder
+        self.agent.feed(trajectory)
+        
 
     def receive_round_start_message(self, round_count, hole_card, seats):
+        # update hole card in state vector
+        self.trajectory = 1 # placeholder
+        self.agent.feed(trajectory)
         pass
 
     def receive_street_start_message(self, street, round_state):
+        # update commnunity cards in state vector
+        self.trajectory = 1 # placeholder
+        self.agent.feed(trajectory)
         pass
 
     def receive_game_update_message(self, action, round_state):
+        # this is called upon every player action
+        self.trajectory = 1 # placeholder
+        self.agent.feed(trajectory)
         pass
 
     def receive_round_result_message(self, winners, hand_info, round_state):
         is_winner = self.uuid in [item['uuid'] for item in winners]
         self.wins += int(is_winner)
         self.losses += int(not is_winner)
+        # pass this data
+        self.agent.feed(trajectory)
+        # learn based of round result
+        self.agent.train_sl()
 
     
 def setup_ai():
