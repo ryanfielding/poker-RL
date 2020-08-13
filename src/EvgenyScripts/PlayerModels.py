74db1002f090debbe94dbd2447d3886f37a07a82
Ryan Fielding Colab <rafieldi@sfu.ca>
2020-08-13 00:41:36 +0000

    minor changes, training

--- src/EvgenyScripts/PlayerModels.py
+++ src/EvgenyScripts/PlayerModels.py
@@ -126,3 +126,94 @@ class RandomPlayer(BasePokerPlayer):
 
     def receive_round_result_message(self, winners, hand_info, round_state):
         pass
+
+
+'''
+  MonteCarloBot courtesy of Victor Velev on Github.
+'''
+
+# Estimate the ratio of winning games given the current state of the game
+def estimate_win_rate(n_simulations, n_players, hole_card, community_card=None):
+    if not community_card: community_card = []
+
+    # Make lists of Card objects out of the list of cards
+    community_card = gen_cards(community_card)
+    hole_card = gen_cards(hole_card)
+
+    # Estimate the win count by doing a Monte Carlo simulation
+    win_count = sum([montecarlo_simulation(n_players, hole_card, community_card) for _ in range(n_simulations)])
+    return 1.0 * win_count / n_simulations
+
+
+def montecarlo_simulation(n_players, hole_card, community_card):
+    # Do a Monte Carlo simulation given the current state of the game by evaluating the hands
+    community_card = _fill_community_card(community_card, used_card=hole_card + community_card)
+    unused_cards = _pick_unused_card((n_players - 1) * 2, hole_card + community_card)
+    opponents_hole = [unused_cards[2*i:2*i+2] for i in range(n_players - 1)]
+    opponents_score = [HandEvaluator.eval_hand(hole, community_card) for hole in opponents_hole]
+    my_score = HandEvaluator.eval_hand(hole_card, community_card)
+    return 1 if my_score >= max(opponents_score) else 0
+
+
+class MonteCarloBot(BasePokerPlayer):
+    def __init__(self):
+        super().__init__()
+        self.wins = 0
+        self.losses = 0
+        self.n_simulations = 1000
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        # Estimate the win rate
+        win_rate = estimate_win_rate(self.n_simulations, self.n_players, hole_card, round_state['community_card'])
+
+        # Check whether it is possible to call
+        can_call = len([item for item in valid_actions if item['action'] == 'call']) > 0
+        if can_call:
+            # If so, compute the amount that needs to be called
+            call_amount = [item for item in valid_actions if item['action'] == 'call'][0]['amount']
+        else:
+            call_amount = 0
+
+        amount = None
+
+        # If the win rate is large enough, then raise
+        if win_rate > 0.5:
+            raise_amount_options = [item for item in valid_actions if item['action'] == 'raise'][0]['amount']
+            if win_rate > 0.85:
+                # If it is extremely likely to win, then raise as much as possible
+                action = 'raise'
+                amount = raise_amount_options['max']
+            elif win_rate > 0.75:
+                # If it is likely to win, then raise by the minimum amount possible
+                action = 'raise'
+                amount = raise_amount_options['min']
+            else:
+                # If there is a chance to win, then call
+                action = 'call'
+        else:
+            action = 'call' if can_call and call_amount == 0 else 'fold'
+
+        # Set the amount
+        if amount is None:
+            items = [item for item in valid_actions if item['action'] == action]
+            amount = items[0]['amount']
+
+        return action, amount
+
+    def receive_game_start_message(self, game_info):
+        self.n_players = game_info['player_num']
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        is_winner = self.uuid in [item['uuid'] for item in winners]
+        self.wins += int(is_winner)
+        self.losses += int(not is_winner)
+
