dd0224f659b94b6dddfe3126f39c3132d84e7b3b
Ryan Fielding <rafieldi@sfu.ca>
2020-08-14 22:31:09 -0700

    simulate script - 50 games against multiple bots

--- src/simulate.py
+++ src/simulate.py
@@ -1,21 +1,56 @@
-from pypokerengine.api.game import start_poker, setup_config
+from pypokerengine.players import BasePokerPlayer
+import EvgenyScripts.PlayerModels as pm
 
-from bots.ai_bot import MonteCarloBot
-from bots.callbot import CallBot
+import sys
+sys.path.insert(0, './cache/')
+sys.path.insert(1, './EvgenyScripts/')
+
+import bots.DQNPlayer1v1 as dqn
+
+from pypokerengine.api.game import setup_config, start_poker
+import matplotlib.pyplot as plt
+import seaborn
+seaborn.set()
 import numpy as np
 
-if __name__ == '__main__':
-    blogger_bot = MonteCarloBot()
+p1 = dqn.DQNPlayer()
 
-    # The stack log contains the stacks of the Data Blogger bot after each game (the initial stack is 100)
-    stack_log = []
-    for round in range(1000):
-        p1, p2 = blogger_bot, CallBot()
+config = setup_config(max_round=100, initial_stack=1000, small_blind_amount=15)
+config.register_player(name="p1", algorithm=p1)
+numGames = 50
+opponents = [pm.HonestPlayer(), pm.MonteCarloBot(), pm.RandomPlayer(), pm.CallPlayer(), pm.HeuristicPlayer()]
+xlabels =('vs HonestPlayer','vs MonteCarlo','vs RandomPlayer','vs CallPlayer','vs HeuristicPlayer')
+plotdir = './plots/'
+p1Wins_list = []
+p2Wins_list = []
 
-        config = setup_config(max_round=5, initial_stack=100, small_blind_amount=5)
-        config.register_player(name="p1", algorithm=p1)
-        config.register_player(name="p2", algorithm=p2)
+for opponent in range(len(opponents)):
+    p1Wins = 0
+    p2Wins = 0
+    config = setup_config(max_round=100, initial_stack=1000, small_blind_amount=15)
+    config.register_player(name="p1", algorithm=p1)
+    config.register_player(name="p2", algorithm=opponents[opponent])
+    for game in range(numGames):
+        print('game: ', game)
         game_result = start_poker(config, verbose=0)
+        print(game_result)
 
-        stack_log.append([player['stack'] for player in game_result['players'] if player['uuid'] == blogger_bot.uuid])
-        print('Avg. stack:', '%d' % (int(np.mean(stack_log))))
+        x = ['RLPlayer', 'CPU']
+        if game_result['players'][0]['stack'] > game_result['players'][1]['stack'] :
+            p1Wins += 1
+        else:
+            p2Wins += 1
+    p1Wins_list.append(p1Wins)
+    p2Wins_list.append(p2Wins)
+
+ind = np.arange(len(opponents)) 
+width = 0.35       
+plt.bar(ind, p1Wins_list, width, label='RLPlayer')
+plt.bar(ind + width, p2Wins_list, width, label='Bot')
+plt.ylabel('Num. of Wins')
+plt.title('50 Games - RLPlayer vs Bots')
+
+plt.xticks(ind + width / 2, xlabels, rotation=12)
+plt.legend(loc='best')
+plt.savefig(plotdir+'Simulate.png')
+plt.show()
